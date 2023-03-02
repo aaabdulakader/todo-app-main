a4678a22ab502611b03ad8cef3e3509c481fea36
Abdulkader Abdi <abdulkaderabdi@Abdulkaders-MacBook-Pro.local>
2023-03-02 12:45:58 -0800

    File relocations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "375px",
+      lg: "1440px",
+    },
     extend: {
-      screens: {
-        'sm': '375px',
-        'lg': '1440px',
-      },
       colors: {
         // primary: {
         //   'bright-blue': 'hsl(220, 98%, 61%)',
@@ -30,38 +30,38 @@ module.exports = {
         // }
 
         primary: {
-          'blue': 'hsl(220, 98%, 61%)',
-          'check-bg': 'linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))'
+          blue: "hsl(220, 98%, 61%)",
+          "check-bg":
+            "linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
         },
         neutral: {
-          'gray-100': 'hsl(0, 0%, 98%)',
-          'gray-200': 'hsl(236, 33%, 92%)',
-          'gray-300': 'hsl(233, 11%, 84%)',
-          'gray-400': 'hsl(236, 9%, 61%)',
-          'gray-500': 'hsl(235, 19%, 35%)'
+          "gray-100": "hsl(0, 0%, 98%)",
+          "gray-200": "hsl(236, 33%, 92%)",
+          "gray-300": "hsl(233, 11%, 84%)",
+          "gray-400": "hsl(236, 9%, 61%)",
+          "gray-500": "hsl(235, 19%, 35%)",
         },
         dark: {
-          'blue-900': 'hsl(235, 21%, 11%)',
-          'blue-800': 'hsl(235, 24%, 19%)',
-          'gray-300': 'hsl(234, 39%, 85%)',
-          'gray-200': 'hsl(236, 33%, 92%)',
-          'gray-500': 'hsl(234, 11%, 52%)',
-          'gray-700': 'hsl(233, 14%, 35%)',
-          'gray-800': 'hsl(237, 14%, 26%)'
-        }
-      }
-      
+          "blue-900": "hsl(235, 21%, 11%)",
+          "blue-800": "hsl(235, 24%, 19%)",
+          "gray-300": "hsl(234, 39%, 85%)",
+          "gray-200": "hsl(236, 33%, 92%)",
+          "gray-500": "hsl(234, 11%, 52%)",
+          "gray-700": "hsl(233, 14%, 35%)",
+          "gray-800": "hsl(237, 14%, 26%)",
+        },
+      },
     },
     fontFamily: {
-      'sans': ['Josefin Sans', 'sans-serif']
+      sans: ["Josefin Sans", "sans-serif"],
     },
     fontSize: {
-    'body': '18px',
-  },
-  fontWeight: {
-    'normal': 400,
-    'bold': 700
-  },
+      body: "18px",
+    },
+    fontWeight: {
+      normal: 400,
+      bold: 700,
+    },
   },
   plugins: [],
-}
+};
